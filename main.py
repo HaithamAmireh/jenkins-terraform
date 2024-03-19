38c84b5d3c3d585ca16bceae346623679df052fd
Haitham Amireh <70890685+HaithamAmireh@users.noreply.github.com>
2024-03-19 16:46:13 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,36 @@
+from fastapi import FastAPI, Form
+import requests
+from fastapi.responses import HTMLResponse
+from typing import Annotated
+
+app = FastAPI()
+
+
+# Function to trigger Jenkins job
+def trigger_jenkins_job(bucket_name):
+    jenkins_url = (
+        "http://localhost:8080"  # Assuming Jenkins is running locally on port 8080
+    )
+    job_name = "terraform-s3"
+    user_name = "haitham-jenkins"
+    api_token = "1116a1c7b19dd01365998e7f13815227d5"
+
+    url = f"{jenkins_url}/job/{job_name}/buildWithParameters"
+    params = {"token": api_token, "bucket_name": bucket_name}
+
+    response = requests.post(url, params=params, auth=(user_name, api_token))
+
+    if response.status_code == 201:
+        print(f"Jenkins job {job_name} triggered successfully.")
+    else:
+        print(
+            f"Failed to trigger Jenkins job {job_name}. HTTP Code: {response.status_code}"
+        )
+
+
+@app.post("/create_bucket", response_class=HTMLResponse)
+async def create_bucket(bucket_name: Annotated[str, Form()]):
+    print(f"Bucket Name: {bucket_name}")
+    response = f"Bucket Name: {bucket_name}<br>"
+    trigger_jenkins_job(bucket_name)
+    return response
